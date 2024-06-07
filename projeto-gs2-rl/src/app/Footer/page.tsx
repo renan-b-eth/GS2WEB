c09db278fc1913d2f3b4db0218a265cbebedea17
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-07 17:24:49 -0300

    footer mudado

--- projeto-gs2-rl/src/app/Footer/page.tsx
+++ projeto-gs2-rl/src/app/Footer/page.tsx
@@ -9,9 +9,9 @@ const Footer = () => {
   return (
     <footer className="App-footer" id="equipe">
       <div className="content-card">
-                    <div className="card-integrantes"><Image src={Renan} alt="Landscape picture" width={250} height={250} className='card efeito2'/></div>
+                    <div className="card-integrantes"><Image src={Renan} alt="Landscape picture" width={250} height={250} className='tamanho2 efeito2'/></div>
                     <div className="descricao"><p>Renan Bezerra dos Santos</p><p>RM - 553228 - 1TDSPB</p></div>
-                    <div className="card-integrantes"><Image src={Lucas} alt="Landscape picture" width={250} height={250} className='card efeito2'/></div>
+                    <div className="card-integrantes"><Image src={Lucas} alt="Landscape picture" width={250} height={250} className='tamanho2 efeito2'/></div>
                     <div className="descricao"><p>Lucas Alcântara Carvalho</p><p>RM - 95111 - 1TDSPB</p></div>
         </div>
 
